8d288625d714a57888c8214e87c6b0ae53626328
vitamin-0xff <essid101010@gmail.com>
2026-01-15 17:48:32 +0100

    add note for aditional params

--- src/content/views/FetchPatternDetection.tsx
+++ src/content/views/FetchPatternDetection.tsx
@@ -1,6 +1,6 @@
 import { PickedElement } from "./types";
 import { useElementPicker } from "./useElementPicker";
-import { useState } from "react"; 
+import { useState } from "react";
 
 type Propos = {
     elementPicked?: PickedElement;
@@ -14,12 +14,15 @@ export const FetchPatternDetection = () => {
     const [isPickingElement, setIsPickingElement] = useState(false);
     const [baseUrl, setBaseUrl] = useState('');
     const [pageParam, setPageParam] = useState('');
+    const [otherParams, setOtherParams] = useState<string[] | null>(null);
+
     const handleElementPicked = (element: PickedElement) => {
         setIsPickingElement(false);
         setPageParam('');
         setBaseUrl('');
+        setOtherParams(null);
         if (!element) return;
-        if(!element.href) {
+        if (!element.href) {
             LOG('Picked element has no href, cannot detect pattern.', element);
             return;
         };
@@ -34,6 +37,13 @@ export const FetchPatternDetection = () => {
         setBaseUrl(url.origin + url.pathname);
         setPageParam(detectedParam);
         setElementPicked(element);
+        const otherParamsArr: string[] = [];
+        params.forEach((value, key) => {
+            if (key !== detectedParam) {
+                otherParamsArr.push(`${key}=${value}`);
+            }
+        });
+        setOtherParams(otherParamsArr.length > 0 ? otherParamsArr : null);
     }
     const { startPicker } = useElementPicker(handleElementPicked, setIsPickingElement)
     return (
@@ -54,18 +64,26 @@ export const FetchPatternDetection = () => {
                 }}
             >
                 {isPickingElement ? 'Picking... (ESC to cancel)' : 'Pick Element'}
-              </button>
-            <small style={{display: 'block', marginTop: 12}} className="text-sm block">Pattern Detection (should be a link with page param, aka page, p..)</small>
-                <div className="form-group" style={{ marginTop: '10px', padding: '10px', border: '1px solid #ccc', borderRadius: '4px', backgroundColor: '#f9f9f9' }}>
-                    <div className="form-item">
-                        <label className="form-label" htmlFor="base-url">Base Url</label>
-                        <input className="form-input" value={baseUrl} type="text" name="base-url" onChange={(e) => setBaseUrl(e.currentTarget.value)} />
-                    </div>
-                    <div className="form-item">
-                        <label className="form-label" htmlFor="detected-page-param">Detected Page Param</label>
-                        <input className="form-input" value={pageParam} type="text" name="detected-page-param" onChange={(e) => setPageParam(e.currentTarget.value)} />
-                    </div>
+            </button>
+            <small style={{ display: 'block', marginTop: 12 }} className="text-sm block">Pattern Detection (should be a link with page param, aka page, p..)</small>
+            <div className="form-group" style={{ marginTop: '10px', padding: '10px', border: '1px solid #ccc', borderRadius: '4px', backgroundColor: '#f9f9f9' }}>
+                <div className="form-item">
+                    <label className="form-label" htmlFor="base-url">Base Url</label>
+                    <input className="form-input" value={baseUrl} type="text" name="base-url" onChange={(e) => setBaseUrl(e.currentTarget.value)} />
                 </div>
+                <div className="form-item">
+                    <label className="form-label" htmlFor="detected-page-param">Detected Page Param</label>
+                    <input className="form-input" value={pageParam} type="text" name="detected-page-param" onChange={(e) => setPageParam(e.currentTarget.value)} />
+                </div>
+                {
+                    otherParams && otherParams.length > 0 && (
+                        <div className="form-item">
+                            <label className="form-label" htmlFor="detected-page-param">Other Params</label>
+                            <input disabled className="form-input" value={otherParams?.join(",")} type="text" name="detected-page-param" onChange={(e) => setPageParam(e.currentTarget.value)} />
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
